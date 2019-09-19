30f3c44edba1cea43ba81e80e6c145f395641ce0
lucaskt <lucaskt@LCBLAB2110-32.uccob.uc.edu>
2019-09-19 09:17:41 -0400

    end of class 9/19

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -14,12 +14,29 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
-			} 
-			System.out.println("");
+		
+		for (int i = 0; i <= 5; i++) {
+			
+        for (int j = 0; j < i; j++) {
+        	System.out.print(""+ i);
+            }
+            System.out.println("");
+        
 		}
+		
 	}
+
 }
+
+// https://javaconceptoftheday.com/how-to-create-pyramid-of-numbers-in-java/
+
+
+/*
+public static void main(String[] args) {
+int lines = 5;
+for (int i = 0; i < lines; i++) {
+	for (int j = 0; j < i+1; j++) {
+		System.out.print(j);
+	}
+	System.out.println("");
+*/
